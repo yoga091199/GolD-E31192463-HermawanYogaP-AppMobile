e2c1d3e2dc571f2829e379a6e375318211cd5c9d
yoga091199 <65710065+yoga091199@users.noreply.github.com>
2021-06-09 21:01:56 +0700

    Tugas API Volley

---
+++ API_Volley/app/src/main/java/com/example/api_volley/Model/ModelData.java
@@ -0,0 +1,29 @@
+package com.example.api_volley.Model;
+
+public class ModelData {String username, grup, nama, password;
+
+    public ModelData(){}
+
+    public ModelData(String username, String grup, String nama, String password){
+        this.username = username;
+        this.grup = grup;
+        this.nama = nama;
+        this.password = password;
+    }
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getNama() {return nama;}
+
+    public void setNama(String nama) {this.nama = nama;}
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getGrup() {return grup;}
+
+    public void setGrup(String grup) {this.grup = grup;}
+}
+
---
+++ API_Volley/app/src/main/java/com/example/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.246.107/Framework/Minggu11/CI/index.php/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.246.107/Framework/Minggu11/CI/index.php/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.246.107/Framework/Minggu11/CI/index.php/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.246.107/Framework/Minggu11/CI/index.php/Mahasiswa/ApiUpdate";
+}
